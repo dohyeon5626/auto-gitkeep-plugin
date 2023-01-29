01dd02bca7b0276b4b4c41c8c6be4c8d675e6692
dohyeon5626 <kodohyeon71@gmail.com>
2023-01-29 20:12:38 +0900

    Add .gitkeep check logic when opening a project
    - Add ProjectStartActivity
    
    #3

---
+++ src/main/kotlin/com/dohyeon5626/event/ProjectStartActivity.kt
@@ -0,0 +1,16 @@
+package com.dohyeon5626.event
+
+import com.dohyeon5626.service.FileService
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
+
+class ProjectStartActivity : StartupActivity {
+
+    private val fileService = service<FileService>()
+
+    override fun runActivity(project: Project) {
+        fileService.generateGitKeep(project.basePath!!)
+    }
+
+}
