5466ee95d1d6e373ab07eb9423851a3201937e7f
dohyeon5626 <kodohyeon71@gmail.com>
2023-01-29 19:45:42 +0900

    Add generate .gitkeep logic
    - Add generate .gitkeep logic
    - Add get child empty directory logic
    - Add vfs refresh logic
    
    #2

--- src/main/kotlin/com/dohyeon5626/service/impl/FileServiceImpl.kt
+++ src/main/kotlin/com/dohyeon5626/service/impl/FileServiceImpl.kt
@@ -1,11 +1,40 @@
 package com.dohyeon5626.service.impl
 
 import com.dohyeon5626.service.FileService
+import com.intellij.openapi.vfs.LocalFileSystem
+import java.io.File
 
 class FileServiceImpl: FileService {
 
+    private val fileSystem = LocalFileSystem.getInstance()
+
     override fun generateGitKeep(path: String) {
-//        TODO("Not yet implemented")
+        with(File(path).listFiles()!!) {
+            if (isEmpty()) {
+                createFile("$path/.gitkeep")
+            } else if (any { it.name != ".gitkeep" }) {
+                filter { it.name == ".gitkeep" }
+                    .forEach { deleteFile(it) }
+
+                filter { it.isDirectory }
+                    .forEach { generateGitKeep(it.path) }
+            }
+        }
+    }
+
+    private fun createFile(path: String) {
+        File(path).apply { createNewFile() }
+            .also { refreshFilePath(it) }
+    }
+
+    private fun deleteFile(file: File) {
+        file.delete()
+        refreshFilePath(file)
+    }
+
+    private fun refreshFilePath(file: File) {
+        fileSystem.refreshAndFindFileByIoFile(file)
+            ?.refresh(true, false)
     }
 
 }
